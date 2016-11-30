c85565239abdc4970d04eaed69b45565a5203645
Shannon Houck <shannonhouck@Shannons-MacBook-Pro.local>
2016-11-30 18:05:17 -0500

    Adding like all of the files ever

---
+++ intro/main.cpp
@@ -0,0 +1,15 @@
+#include "molecule.h"
+
+using namespace std
+
+int main(int argc, char* argv[])
+{
+	# create new Molecule
+	Molecule h2o = new Molecule();
+
+	# set coordinates of Molecule
+	h2o.geom[
+
+	return 0;
+}
+
